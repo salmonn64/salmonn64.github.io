591e4570b3bd4de3b6c5801c2e83ac1c5e36f7ca
José David García Díaz <david_diaz@ciencias.unam.mx>
2023-05-04 13:41:12 -0600

    Added links to the game  subpages.

--- jss/script.js
+++ jss/script.js
@@ -1,5 +1,6 @@
 function showMail(){
     document.getElementById("mail").style.display='block';
+    document.getElementById("mail").style = 'stylesheet';
     var audioEffect = new Audio("resources/fairy.wav");
     audioEffect.volume = .2;
     audioEffect.play();
